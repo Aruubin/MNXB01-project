f9c6505f013f9a67b28c3c154690aada5616d94c
Thomas <thomas.anthony.eriksson@gmail.com>
2018-11-05 20:33:08 +0000

    Read the complete data file in the constructor.
    Managed to read in the file. The average year temperature was
    obtained but still stored in a local varible in constructor.

--- Thomas/tempTrender.cpp
+++ Thomas/tempTrender.cpp
@@ -6,6 +6,18 @@ using namespace std;
 #include "tempTrender.h"
 
 tempTrender::tempTrender(string filePath) {
+	const Int_t startYear = 1961;
+	const Int_t endYear = 2014;
+	const Int_t numberOfYears = endYear-startYear+1;
+	Double_t yearSum[numberOfYears];
+	Int_t yearCount[numberOfYears];
+	
+	for(Int_t i = 0; i < numberOfYears; i++)
+	{
+		yearSum[i] = 0.0;
+		yearCount[i] = 0;
+	}
+	
 	ifstream file(filePath.c_str());
 	string helpString;
 	while(file >> helpString)
@@ -16,17 +28,58 @@ tempTrender::tempTrender(string filePath) {
 		}
 		
 	}
+	
 	while(file >> helpString)
-	{
-		string firstTwo = helpString.substr(0,2);
-		if (firstTwo == "19" || firstTwo == "20")
+	{ 
+		
+		if (helpString.length()>24)
 		{
-			string date = helpString.substr(0,10);
-			string hour = helpString.substr(11,2);
-			string rest = helpString.substr(20);
+			string firstTwo = helpString.substr(0,2);
+			if (firstTwo == "19" || firstTwo == "20")
+			{	
+				string dateStr = helpString.substr(0,10);
+				string yearStr = helpString.substr(0,4);
+				string hourStr = helpString.substr(11,2);
+				string restStr = helpString.substr(20);
+
+				Int_t semicolon_pos = restStr.find(";");
+				string tempStr = restStr.substr(0,semicolon_pos); 
+				string qualityStr = restStr.substr(semicolon_pos+1,1);
+				if(qualityStr == "G")
+				{
+					if(hourStr == "06" || hourStr == "18")
+					{
+						Int_t year;
+						Float_t temp;
+						sscanf(yearStr.c_str(), "%d", &year);
+						sscanf(tempStr.c_str(), "%f", &temp);
+						if (year >= startYear && year <= endYear)
+						{
+							Int_t pos = year-startYear;
+							yearSum[pos] += temp;
+							yearCount[pos] += 1;
+						}
+						
+						//cout << "Year: " << year << " Temp: " << temp << endl;
+					}
+				}
+				
+			}
+		
 			
-			cout << date << endl;
 		} 
+	}
+	
+	Double_t yearAvg[numberOfYears];
+	for(Int_t i = 0; i < numberOfYears; i++)
+	{
+		yearAvg[i] = yearSum[i]/yearCount[i];
+		
+	}
+	
+	for (Int_t i = 0; i<numberOfYears; i++)
+	{
+		cout << yearAvg[i] << endl;
 	}
 	
 	//cout << "The user supplied " << filePath << " as the path to the data file." << endl;
