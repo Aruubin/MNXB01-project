8da80304d847083224ac58267b6076dea76b97e8
Albin Jonsson <al0514jo-s@student.lu.se>
2018-11-11 12:24:12 +0100

    Created folder for final codes and main script to call others

---
+++ Finalised_code/Main_script.C
@@ -0,0 +1,9 @@
+//Here we will include the files used for our tasks and call them
+
+#include "ReadVisby.C"
+
+void project(){
+	
+	Visby_hist(5); //Here the investigation of coastal versus inland climate is done.
+					//In this case, the investigated month is May
+}
